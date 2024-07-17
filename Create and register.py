e6db1b323ff9f6b8a37fb9cfd3d31e198c13c6be
t-bence <t-bence>
2024-07-17 07:36:52 +0000

    Create and register done

---
+++ Create and register.py
@@ -0,0 +1,54 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC Let's create a model and register it, so we can serve it from Databricks model serving
+
+# COMMAND ----------
+
+import mlflow
+mlflow.set_registry_uri("databricks-uc")
+
+# COMMAND ----------
+
+import mlflow.pyfunc
+
+# COMMAND ----------
+
+class Model(mlflow.pyfunc.PythonModel):
+    def predict(self, context, model_input):
+        return 5
+    
+
+# COMMAND ----------
+
+model = Model()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Generate some dummy signature, as it is needed to register the model
+
+# COMMAND ----------
+
+from mlflow.models.signature import infer_signature
+import pandas as pd
+
+signature = infer_signature(
+  pd.DataFrame(((0., 1.),), columns=["x", "y"]),
+  pd.DataFrame(((0.,),), columns=["prediction"])
+)
+
+# COMMAND ----------
+
+mlflow.set_experiment(f"/Users/bence.toth@datapao.com/Dummy-Model-Tracking")
+
+with mlflow.start_run() as run:
+    mlflow.pyfunc.log_model(
+        "model",
+        python_model=model,
+        signature=signature,
+        registered_model_name="bence-toth-catalog.testing.dummy-model"
+    )
+
+# COMMAND ----------
+
+
